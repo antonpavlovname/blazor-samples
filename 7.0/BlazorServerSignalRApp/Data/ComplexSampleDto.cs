326a19993afac26f99f7782b5f17d61edc6db196
Anton Pavlov <sourceforge@antonpavlov.name>
2023-11-03 17:00:33 +0100

    Add complex signalR dto example

---
+++ 7.0/BlazorServerSignalRApp/Data/ComplexSampleDto.cs
@@ -0,0 +1,5 @@
+﻿namespace BlazorServerSignalRApp.Data;
+
+public record TestItem(int Id, string Name);
+public record ComplexSampleRequest(string Title, string Description, List<TestItem> Items);
+public record ComplexSampleResponse(bool Success, TestItem? SelectedItem);
--- 7.0/BlazorServerSignalRApp/Hubs/ChatHub.cs
+++ 7.0/BlazorServerSignalRApp/Hubs/ChatHub.cs
@@ -1,11 +1,52 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using BlazorServerSignalRApp.Data;
+using Microsoft.AspNetCore.SignalR;
 
 namespace BlazorServerSignalRApp.Server.Hubs;
 
 public class ChatHub : Hub
 {
+    private static string? lastConnected;
+    public override Task OnConnectedAsync()
+    {
+        lastConnected = Context.ConnectionId;
+        return base.OnConnectedAsync();
+    }
+
+    public override Task OnDisconnectedAsync(Exception? exception)
+    {
+        if (lastConnected != Context.ConnectionId)
+        {
+            lastConnected = null;
+        }
+        return base.OnDisconnectedAsync(exception);
+    }
+
     public async Task SendMessage(string user, string message)
     {
         await Clients.All.SendAsync("ReceiveMessage", user, message);
+    }
+
+    public async Task WaitMessage()
+    {
+        var clientId = lastConnected ?? Context.ConnectionId;
+        var message = await Clients.Client(clientId).InvokeAsync<string>("GetMessage", CancellationToken.None);
+        Console.WriteLine($"Got message {message}");
+    }
+
+    public async Task<string> WaitMessageWithPrompt(string prompt)
+    {
+        var clientId = lastConnected ?? Context.ConnectionId;
+        var message = await Clients.Client(clientId).InvokeAsync<string>("GetMessageWithPrompt", $"Input {prompt}", CancellationToken.None);
+        Console.WriteLine($"Got echo {message}");
+        return message;
+    }
+
+    public async Task ComplexMessage()
+    {
+        var clientId = lastConnected ?? Context.ConnectionId;
+        var request = new ComplexSampleRequest("Test title", "Test description",
+            new List<TestItem> { new(777, "Tri topora"), new(44, "Sorok cetverty") });
+        var response = await Clients.Client(clientId).InvokeAsync<ComplexSampleResponse>("ComplexPrompt", request, CancellationToken.None);
+        Console.WriteLine($"Got echo {response}");
     }
 }
